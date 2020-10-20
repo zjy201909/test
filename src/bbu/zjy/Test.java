38fff4a19b86d71427f2436353ad1dc1e635161f
zjy201909 <73155883+zjy201909@users.noreply.github.com>
2020-10-20 16:26:33 +0800

    Update Test.java

--- src/bbu/zjy/Test.java
+++ src/bbu/zjy/Test.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Test {
 	public static void main(String[] args) {
 		ArrayList<String> list = new ArrayList<String>();
-		list.add("hello world v0.1 merge github update");
+		list.add("hello world v0.1 merge github update agign");
 		System.out.println(list.get(0));
 	}
 }
