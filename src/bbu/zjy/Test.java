625779b2dca596cacc08df7a16ca4994d9972652
zjy <1934073582@qq.com>
2020-10-20 14:19:36 +0800

    new git

---
+++ src/bbu/zjy/Test.java
@@ -0,0 +1,11 @@
+package bbu.zjy;
+
+import java.util.ArrayList;
+
+public class Test {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("hello world");
+		System.out.println(list.get(0));
+	}
+}
